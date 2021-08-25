9a58aa41a372f6b102ccf5aaac0d55ad2cb80954
Ken-muigai <kenmuigai980@gmail.com>
2021-08-25 08:16:18 -0400

    initial commits

---
+++ src/main/java/com/kenjoel/soso/soso/SosoApplication.java
@@ -0,0 +1,13 @@
+package com.kenjoel.soso.soso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenjoel/soso/soso/SosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenjoel.soso.soso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
